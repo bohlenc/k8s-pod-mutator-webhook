9207b11a7cbc36a81f493f0023d37e56053c6dd1
Christopher Bohlen <christopher.bohlen@codecentric.de>
2021-03-04 21:35:56 +0100

    fixes x509: certificate private key does not implement crypto.Signer: pass keys by ref

--- pkg/cert-generator/cert_generator_test.go
+++ pkg/cert-generator/cert_generator_test.go
@@ -1,3 +1,23 @@
 package cert_generator
 
-// TODO
+import (
+	"github.com/stretchr/testify/assert"
+	"k8s-pod-mutator-webhook/pkg/webhook"
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	_, err := Generate(
+		webhook.ServiceMetadata{
+			Name:      "some-service-name",
+			Namespace: "some-service-namespace",
+		},
+		CertOutputFiles{
+			CaCertOutputFile:  "/tmp/ca.crt",
+			CaKeyOutputFile:   "/tmp/ca.key",
+			TlsCertOutputFile: "/tmp/tls.crt",
+			TlsKeyOutputFile:  "/tmp/tls.key",
+		},
+	)
+	assert.Nil(t, err)
+}
